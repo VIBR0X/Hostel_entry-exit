592c2ddc31e4a1b1b351ab8878292122858cfc14
ananyac9 <ananyachavadhal@gmail.com>
2023-02-23 21:27:28 +0530

    .

--- hostelentryexit/hostelentryexit/models.py
+++ hostelentryexit/hostelentryexit/models.py
@@ -1,15 +1,9 @@
 from django.db import models
 
 class Hostel (models.Model):
+    h_num = models.IntegerField(max_length=4)
     username = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
 
-from django.db import forms
-
-class HostelForm (forms.ModelForm):
-    password = forms.CharField (widget = forms. PasswordInput)
-
-    class Meta:
-        model = Hostel
 
 
