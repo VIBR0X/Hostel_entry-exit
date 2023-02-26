11e3b4cfdc962733024e4e0432f4e8ec128ed88a
ananyac9 <ananyachavadhal@gmail.com>
2023-02-26 16:15:39 +0530

    hostel model and API made

--- hostelentryexit/hostelentryexit/views.py
+++ hostelentryexit/hostelentryexit/views.py
@@ -29,12 +29,14 @@ def hostel_detail(request, id, format=None):
     if request.method == 'GET':
         serializer = HostelSerializer(hostel)
         return Response(serializer.data)
+    
     elif request.method == 'PUT':
         serializer = HostelSerializer(hostel, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
     elif request.method == 'DELETE':
         hostel.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
